cdefaeb2057850261d35a31823c3ff4abf976891
Phillip Henry III <philtrader71@gmail.com>
2019-12-28 18:37:17 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
 new_arr = []
   counter = 0
   while counter < array.length do
-    p new_arr << array[counter] ** 2
+    pp new_arr << array[counter] ** 2
     counter += 1
 
   end
